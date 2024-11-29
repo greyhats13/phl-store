da04dc39d68ebbbb9bc90778aa74dd7a4505861b
greyhats13 <imam.arief.rhmn@gmail.com>
2024-11-29 22:20:03 +0700

    add s3 and kms

---
+++ iac/deployment/cloud/main.tf
@@ -0,0 +1,44 @@
+# Create General purpose KMS key
+module "kms_main" {
+  source                = "terraform-aws-modules/kms/aws"
+  version               = "~> 3.1.1"
+  aliases               = ["main/${local.kms_naming_standard}"]
+  description           = "${local.kms_naming_standard} cluster encryption key"
+  enable_default_policy = true
+  key_owners            = ["arn:aws:iam::124456474132:role/iac"]
+  key_users             = ["arn:aws:iam::124456474132:user/iac"]
+  key_service_roles_for_autoscaling = [
+    # required for the ASG to manage encrypted volumes for nodes
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
+    # required for the cluster / persistentvolume-controller to create encrypted PVCs
+    # module.eks_main.cluster_iam_role_arn
+  ]
+  tags = merge(local.tags, local.kms_standard, { Name = local.kms_naming_standard })
+}
+
+# Create S3 bucket for terraform state
+module "bucket_tfstate" {
+  source                   = "terraform-aws-modules/s3-bucket/aws"
+  version                  = "~> 4.2.2"
+  bucket                   = local.s3_naming_standard
+  acl                      = "private"
+  force_destroy            = true
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+  attach_policy            = true
+  policy                   = data.aws_iam_policy_document.custom_bucket_policy.json
+  expected_bucket_owner    = data.aws_caller_identity.current.account_id
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = module.kms_main.key_arn
+      }
+    }
+  }
+  versioning = {
+    enabled = true
+  }
+
+  tags = merge(local.tags, local.s3_standard, { Name = local.s3_naming_standard })
+}
