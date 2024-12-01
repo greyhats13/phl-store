80f3c986334df13a1e471febeb2e8636ab774635
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-01 22:00:00 +0700

    add vpc module

--- iac/deployment/cloud/outputs.tf
+++ iac/deployment/cloud/outputs.tf
@@ -6,3 +6,12 @@ output "main_key_id" {
 output "main_key_arn" {
   value = module.kms_main.key_arn
 }
+
+# S3 Outputs
+output "tfstate_bucket_id" {
+  value = module.bucket_tfstate.s3_bucket_id
+}
+
+output "tfstate_bucket_arn" {
+  value = module.bucket_tfstate.s3_bucket_arn
+}
