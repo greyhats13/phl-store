a7a3ac08a85ccaf666b89cb6fd480685054b8346
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-08 21:46:17 +0700

    add atlantis module 2

--- iac/deployment/cloud/main.tf
+++ iac/deployment/cloud/main.tf
@@ -477,6 +477,24 @@ module "aws_ebs_csi_pod_identity" {
   tags = local.tags
 }
 
+# Create Storage Class for gp3
+resource "kubernetes_storage_class_v1" "gp3" {
+  metadata {
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  reclaim_policy      = "Delete"
+  parameters = {
+    type       = "gp3"
+    iops       = "3000"
+    throughput = "125"
+  }
+  volume_binding_mode = "WaitForFirstConsumer"
+}
+
 module "aws_cloudwatch_observability_pod_identity" {
   source  = "terraform-aws-modules/eks-pod-identity/aws"
   version = "~> 1.7.0"
