53251dadb7782b8cfa7fbb4cfe0a0cecea172655
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-09 02:51:21 +0700

    test destroy phl-products

--- iac/deployment/services/phl-products/main.tf
+++ iac/deployment/services/phl-products/main.tf
@@ -1,69 +1,69 @@
 # Create a Database
-resource "mysql_database" "db" {
-  name = local.svc_naming_standard
-}
+# resource "mysql_database" "db" {
+#   name = local.svc_naming_standard
+# }
 
-resource "mysql_user" "db" {
-  user               = local.svc_naming_standard
-  host               = "%"
-  plaintext_password = random_password.password.result
-}
+# resource "mysql_user" "db" {
+#   user               = local.svc_naming_standard
+#   host               = "%"
+#   plaintext_password = random_password.password.result
+# }
 
-resource "mysql_grant" "db" {
-  user       = mysql_user.db.user
-  host       = mysql_user.db.host
-  database   = mysql_database.db.name
-  privileges = ["SELECT", "INSERT", "UPDATE", "DELETE"]
-}
+# resource "mysql_grant" "db" {
+#   user       = mysql_user.db.user
+#   host       = mysql_user.db.host
+#   database   = mysql_database.db.name
+#   privileges = ["SELECT", "INSERT", "UPDATE", "DELETE"]
+# }
 
-# Create Secrets Manager
-module "secrets_iac" {
-  source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "~> 1.3.1"
+# # Create Secrets Manager
+# module "secrets_iac" {
+#   source  = "terraform-aws-modules/secrets-manager/aws"
+#   version = "~> 1.3.1"
 
-  # Secret
-  name                    = local.svc_secret_standard
-  description             = "Secrets for ${local.svc_secret_standard}"
-  recovery_window_in_days = 0
+#   # Secret
+#   name                    = local.svc_secret_standard
+#   description             = "Secrets for ${local.svc_secret_standard}"
+#   recovery_window_in_days = 0
 
-  # Policy
-  create_policy       = true
-  block_public_policy = true
-  policy_statements = {
-    admin = {
-      sid = "IacSecretAdmin"
-      principals = [
-        {
-          type        = "AWS"
-          identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
-        },
-        {
-          type        = "AWS"
-          identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/imam.arief.rhmn@gmail.com"]
-        },
-        {
-          type        = "AWS"
-          identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/idanfreak@gmail.com"]
-        },
-        {
-          type        = "AWS"
-          identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/iac"]
-        },
-      ]
-      actions   = ["secretsmanager:GetSecretValue"]
-      resources = ["*"]
-    }
-  }
+#   # Policy
+#   create_policy       = true
+#   block_public_policy = true
+#   policy_statements = {
+#     admin = {
+#       sid = "IacSecretAdmin"
+#       principals = [
+#         {
+#           type        = "AWS"
+#           identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+#         },
+#         {
+#           type        = "AWS"
+#           identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/imam.arief.rhmn@gmail.com"]
+#         },
+#         {
+#           type        = "AWS"
+#           identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/idanfreak@gmail.com"]
+#         },
+#         {
+#           type        = "AWS"
+#           identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/iac"]
+#         },
+#       ]
+#       actions   = ["secretsmanager:GetSecretValue"]
+#       resources = ["*"]
+#     }
+#   }
 
-  # Version
-  ignore_secret_changes = false
-  secret_string = jsonencode({
-    connection_string = "${mysql_user.db.user}:${random_password.password.result}@tcp(${data.terraform_remote_state.cloud.outputs.aurora_cluster_endpoint}:${data.terraform_remote_state.cloud.outputs.aurora_cluster_port})/${mysql_database.db.name}"
-    port              = "8080"
-  })
+#   # Version
+#   ignore_secret_changes = false
+#   secret_string = jsonencode({
+#     connection_string = "${mysql_user.db.user}:${random_password.password.result}@tcp(${data.terraform_remote_state.cloud.outputs.aurora_cluster_endpoint}:${data.terraform_remote_state.cloud.outputs.aurora_cluster_port})/${mysql_database.db.name}"
+#     port              = "8080"
+#   })
 
-  tags = merge(local.tags, local.svc_standard)
-}
+#   tags = merge(local.tags, local.svc_standard)
+# }
 
 # # ArgoCD Vault Plugin (AVP) Pod Identity
 # module "svc_custom_pod_identity" {
