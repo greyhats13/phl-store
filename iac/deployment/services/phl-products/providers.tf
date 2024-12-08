fc21deea8dd5498f40e28ab31444336b5ed7ccbd
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-09 04:05:27 +0700

    add phl-product 7

--- iac/deployment/services/phl-products/providers.tf
+++ iac/deployment/services/phl-products/providers.tf
@@ -58,12 +58,13 @@ provider "aws" {
 provider "kubernetes" {
   host                   = data.terraform_remote_state.cloud.outputs.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(data.terraform_remote_state.cloud.outputs.eks_cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.cloud.outputs.eks_cluster_name]
-  }
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  # exec {
+  #   api_version = "client.authentication.k8s.io/v1beta1"
+  #   command     = "aws"
+  #   # This requires the awscli to be installed locally where Terraform is executed
+  #   args = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.cloud.outputs.eks_cluster_name]
+  # }
 }
 
 # Create Helm provider
@@ -71,12 +72,13 @@ provider "helm" {
   kubernetes {
     host                   = data.terraform_remote_state.cloud.outputs.eks_cluster_endpoint
     cluster_ca_certificate = base64decode(data.terraform_remote_state.cloud.outputs.eks_cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.cloud.outputs.eks_cluster_name]
-    }
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    # exec {
+    #   api_version = "client.authentication.k8s.io/v1beta1"
+    #   command     = "aws"
+    #   # This requires the awscli to be installed locally where Terraform is executed
+    #   args = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.cloud.outputs.eks_cluster_name]
+    # }
   }
   # registry {
   #   url      = "oci://public.ecr.aws"
