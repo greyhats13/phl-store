6f278985a49f3248d36f5a7fc33a0144de9e9273
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-11 04:26:00 +0700

    change params to svc_name

--- iac/deployment/services/phl-products/main.tf
+++ iac/deployment/services/phl-products/main.tf
@@ -123,7 +123,7 @@ module "repo_phl" {
   source                  = "../../../modules/github"
   repo_name               = var.github_repo
   owner                   = var.github_owner
-  environment             = local.svc_naming_standard
+  svc_name                = local.svc_naming_standard
   github_action_variables = local.github_action_variables
   github_action_secrets   = local.github_action_secrets
 }
