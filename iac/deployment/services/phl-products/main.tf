819d6a7289a40e98497a20271acc0f06af4614ae
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-10 23:16:04 +0700

    fix path id

--- iac/deployment/services/phl-products/main.tf
+++ iac/deployment/services/phl-products/main.tf
@@ -201,7 +201,7 @@ module "api_integration_routes" {
         }
         request_parameters = {
           "overwrite:header.Host" = "${local.svc_standard.Feature}.${data.terraform_remote_state.cloud.outputs.dns_name}"
-          "overwrite:path"         = "/api/${local.svc_standard.Feature}"
+          "overwrite:path"         = "/api/${local.svc_standard.Feature}{id}"
         }
       }
     }
@@ -225,7 +225,7 @@ module "api_integration_routes" {
         }
         request_parameters = {
           "overwrite:header.Host" = "${local.svc_standard.Feature}.${data.terraform_remote_state.cloud.outputs.dns_name}"
-          "overwrite:path"         = "/api/${local.svc_standard.Feature}"
+          "overwrite:path"         = "/api/${local.svc_standard.Feature}{id}"
         }
       }
     }
@@ -249,7 +249,7 @@ module "api_integration_routes" {
         }
         request_parameters = {
           "overwrite:header.Host" = "${local.svc_standard.Feature}.${data.terraform_remote_state.cloud.outputs.dns_name}"
-          "overwrite:path"         = "/api/${local.svc_standard.Feature}"
+          "overwrite:path"         = "/api/${local.svc_standard.Feature}/{id}"
         }
         response_parameters = [
           {
