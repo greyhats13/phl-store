4cb58c73a84fa961106362026b7b57a09a09e35a
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-11 06:20:34 +0700

    add new gh env

--- iac/deployment/services/phl-products/locals.tf
+++ iac/deployment/services/phl-products/locals.tf
@@ -16,12 +16,14 @@ locals {
   svc_secret_standard = "${local.svc_standard.Unit}/${local.svc_standard.Code}/${local.svc_standard.Unit}-${local.svc_standard.Feature}"
   ## Environment variables that will be stored in Github repo environment for Github Actions
   github_action_variables = {
-    "${local.svc_standard.Feature}_svc_name" = local.svc_name
-    "gh_repo_name"                           = var.github_repo
-    "gh_owner"                               = "git@github.com:${var.github_owner}/${var.github_repo}.git"
-    "aws_region"                             = var.region
-    "aws_account_id"                         = data.aws_caller_identity.current.account_id
-    "gh_oidc_role_arn"                       = data.terraform_remote_state.cloud.outputs.github_oidc_role_arn
+    "${local.svc_standard.Feature}_svc_name"            = local.svc_name
+    "${local.svc_standard.Feature}_svc_naming_standard" = local.svc_naming_standard
+    "${local.svc_standard.Feature}_svc_naming_full"     = local.svc_naming_full
+    "gh_repo_name"                                      = var.github_repo
+    "gh_owner"                                          = "git@github.com:${var.github_owner}/${var.github_repo}.git"
+    "aws_region"                                        = var.region
+    "aws_account_id"                                    = data.aws_caller_identity.current.account_id
+    "gh_oidc_role_arn"                                  = data.terraform_remote_state.cloud.outputs.github_oidc_role_arn
   }
   ## Environment secrets that will be stored in Github repo environment for Github Actions
   github_action_secrets = {
