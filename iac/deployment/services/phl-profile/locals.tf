9eec55d0ae8d72c05bebfcd5d4783614625f2e4b
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-12 04:40:52 +0700

    release new profile services

--- iac/deployment/services/phl-profile/locals.tf
+++ iac/deployment/services/phl-profile/locals.tf
@@ -16,10 +16,12 @@ locals {
   svc_secret_standard = "${local.svc_standard.Unit}/${local.svc_standard.Code}/${local.svc_standard.Unit}-${local.svc_standard.Feature}"
   ## Environment variables that will be stored in Github repo environment for Github Actions
   github_action_variables = {
-    service_name          = local.svc_name
-    docker_repository_uri = "greyhats13/${local.svc_name}"
-    gitops_repo_name      = var.github_repo
-    repo_gitops_ssh       = "git@github.com:${var.github_owner}/${var.github_repo}.git"
-    gitops_path_dev       = "gitops/charts/app/${local.svc_name}"
+    "${local.svc_standard.Feature}_svc_name"            = local.svc_name
+    "${local.svc_standard.Feature}_svc_naming_standard" = local.svc_naming_standard
+    "${local.svc_standard.Feature}_svc_naming_full"     = local.svc_naming_full
+  }
+  ## Environment secrets that will be stored in Github repo environment for Github Actions
+  github_action_secrets = {
+    argocd_ssh = base64decode(jsondecode(data.aws_secretsmanager_secret_version.argocd_ssh.secret_string)["argocd_ssh_base64"])
   }
 }
