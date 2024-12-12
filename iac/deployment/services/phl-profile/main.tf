e058001ad666298cc65d71cc50dfdf4ad35970f1
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-12 23:42:31 +0700

    delete secret

--- iac/deployment/services/phl-profile/main.tf
+++ iac/deployment/services/phl-profile/main.tf
@@ -21,55 +21,55 @@ resource "mysql_grant" "db" {
 
 # Secrets Manager
 ## Create Secrets Manager
-module "secrets_iac" {
-  source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "~> 1.3.1"
+# module "secrets_iac" {
+#   source  = "terraform-aws-modules/secrets-manager/aws"
+#   version = "~> 1.3.1"
 
-  # Secret
-  name                    = local.svc_secret_standard
-  description             = "Secrets for ${local.svc_secret_standard}"
-  recovery_window_in_days = 0
-  # Policy
-  create_policy       = true
-  block_public_policy = true
-  policy_statements = {
-    admin = {
-      sid = "IacSecretAdmin"
-      principals = [
-        {
-          type        = "AWS"
-          identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
-        },
-        {
-          type        = "AWS"
-          identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/imam.arief.rhmn@gmail.com"]
-        },
-        {
-          type        = "AWS"
-          identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/idanfreak@gmail.com"]
-        },
-        {
-          type        = "AWS"
-          identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/iac"]
-        },
-      ]
-      actions   = ["secretsmanager:GetSecretValue"]
-      resources = ["*"]
-    }
-  }
+#   # Secret
+#   name                    = local.svc_secret_standard
+#   description             = "Secrets for ${local.svc_secret_standard}"
+#   recovery_window_in_days = 0
+#   # Policy
+#   create_policy       = true
+#   block_public_policy = true
+#   policy_statements = {
+#     admin = {
+#       sid = "IacSecretAdmin"
+#       principals = [
+#         {
+#           type        = "AWS"
+#           identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+#         },
+#         {
+#           type        = "AWS"
+#           identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/imam.arief.rhmn@gmail.com"]
+#         },
+#         {
+#           type        = "AWS"
+#           identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/idanfreak@gmail.com"]
+#         },
+#         {
+#           type        = "AWS"
+#           identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/iac"]
+#         },
+#       ]
+#       actions   = ["secretsmanager:GetSecretValue"]
+#       resources = ["*"]
+#     }
+#   }
 
-  # Version
-  ignore_secret_changes = false
-  secret_string = jsonencode({
-    db_host           = data.terraform_remote_state.cloud.outputs.aurora_cluster_endpoint
-    db_port           = tostring(data.terraform_remote_state.cloud.outputs.aurora_cluster_port)
-    db_name           = mysql_database.db.name
-    db_user           = mysql_user.db.user
-    db_password       = random_password.password.result
-  })
+#   # Version
+#   ignore_secret_changes = false
+#   secret_string = jsonencode({
+#     db_host           = data.terraform_remote_state.cloud.outputs.aurora_cluster_endpoint
+#     db_port           = tostring(data.terraform_remote_state.cloud.outputs.aurora_cluster_port)
+#     db_name           = mysql_database.db.name
+#     db_user           = mysql_user.db.user
+#     db_password       = random_password.password.result
+#   })
 
-  tags = merge(local.tags, local.svc_standard)
-}
+#   tags = merge(local.tags, local.svc_standard)
+# }
 
 # CI/CD Components
 module "ecr" {
