dd6d9c5bcbe0e442784c1069e47bcb8e55dd0ec8
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-16 09:45:20 +0700

    add karpenter module

--- iac/deployment/cloud/main.tf
+++ iac/deployment/cloud/main.tf
@@ -761,25 +761,109 @@ module "oidc_github" {
   ]
 }
 
-# # AWS required resources for Karpenter
-# module "eks_karpenter" {
-#   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
-#   version = "~> 20.31.0"
+# Karpenter
+resource "helm_release" "karpenter" {
+  namespace           = "kube-system"
+  name                = "karpenter"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart               = "karpenter"
+  version             = "1.1.0"
+  wait                = false
 
-#   cluster_name = module.eks_main.cluster_name
+  values = [
+    <<-EOT
+    dnsPolicy: Default
+    settings:
+      clusterName: ${module.eks_main.cluster_name}
+      clusterEndpoint: ${module.eks_main.cluster_endpoint}
+      interruptionQueue: ${module.karpenter.queue_name}
+    webhook:
+      enabled: false
+    EOT
+  ]
+}
 
-#   enable_v1_permissions = true
+resource "kubectl_manifest" "karpenter_node_class" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.k8s.aws/v1beta1
+    kind: EC2NodeClass
+    metadata:
+      name: default
+    spec:
+      amiFamily: Bottlerocket
+      role: ${module.karpenter.node_iam_role_name}
+      subnetSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${module.eks_main.cluster_name}
+      securityGroupSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${module.eks_main.cluster_name}
+      tags:
+        karpenter.sh/discovery: ${module.eks_main.cluster_name}
+  YAML
 
-#   enable_pod_identity             = true
-#   create_pod_identity_association = true
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
 
-#   # Used to attach additional IAM policies to the Karpenter node IAM role
-#   node_iam_role_additional_policies = {
-#     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-#   }
+resource "kubectl_manifest" "karpenter_node_pool" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.sh/v1beta1
+    kind: NodePool
+    metadata:
+      name: default
+    spec:
+      template:
+        spec:
+          nodeClassRef:
+            name: default
+          requirements:
+            - key: "karpenter.k8s.aws/instance-category"
+              operator: In
+              values: ["t","c", "m", "r"]
+            - key: "karpenter.k8s.aws/instance-cpu"
+              operator: In
+              values: ["2", "4", "8", "16"]
+            - key: "karpenter.k8s.aws/instance-hypervisor"
+              operator: In
+              values: ["nitro"]
+            - key: "karpenter.k8s.aws/instance-generation"
+              operator: Gt
+              values: ["5"]
+      limits:
+        cpu: 1000
+      disruption:
+        consolidationPolicy: WhenEmpty
+        consolidateAfter: 30s
+  YAML
 
-#   tags = local.tags
-# }
+  depends_on = [
+    kubectl_manifest.karpenter_node_class
+  ]
+}
+
+# AWS required resources for Karpenter
+module "eks_karpenter" {
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "~> 20.31.0"
+
+  cluster_name = module.eks_main.cluster_name
+
+  enable_v1_permissions = true
+
+  enable_pod_identity             = true
+  create_pod_identity_association = true
+
+  # Used to attach additional IAM policies to the Karpenter node IAM role
+  node_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  tags = local.tags
+}
 
 module "cognito_pool" {
   source                       = "../../modules/cognito"
