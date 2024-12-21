5e8ebfa8baefe58dfc2d7403abf775fc3f7dc296
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-21 22:46:35 +0700

    uncommented karpenter

--- iac/deployment/cloud/main.tf
+++ iac/deployment/cloud/main.tf
@@ -762,108 +762,108 @@ module "oidc_github" {
 }
 
 # Karpenter
-# resource "helm_release" "karpenter" {
-#   namespace           = "kube-system"
-#   name                = "karpenter"
-#   repository          = "oci://public.ecr.aws/karpenter"
-#   repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-#   repository_password = data.aws_ecrpublic_authorization_token.token.password
-#   chart               = "karpenter"
-#   version             = "1.1.0"
-#   wait                = false
+resource "helm_release" "karpenter" {
+  namespace           = "kube-system"
+  name                = "karpenter"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart               = "karpenter"
+  version             = "1.1.0"
+  wait                = false
 
-#   values = [
-#     <<-EOT
-#     dnsPolicy: Default
-#     settings:
-#       clusterName: ${module.eks_main.cluster_name}
-#       clusterEndpoint: ${module.eks_main.cluster_endpoint}
-#       interruptionQueue: ${module.karpenter.queue_name}
-#     webhook:
-#       enabled: false
-#     EOT
-#   ]
-# }
+  values = [
+    <<-EOT
+    dnsPolicy: Default
+    settings:
+      clusterName: ${module.eks_main.cluster_name}
+      clusterEndpoint: ${module.eks_main.cluster_endpoint}
+      interruptionQueue: ${module.karpenter.queue_name}
+    webhook:
+      enabled: false
+    EOT
+  ]
+}
 
-# resource "kubectl_manifest" "karpenter_node_class" {
-#   yaml_body = <<-YAML
-#     apiVersion: karpenter.k8s.aws/v1beta1
-#     kind: EC2NodeClass
-#     metadata:
-#       name: autoscaling
-#     spec:
-#       amiFamily: Bottlerocket
-#       role: ${module.karpenter.node_iam_role_name}
-#       subnetSelectorTerms:
-#         - tags:
-#             karpenter.sh/discovery: ${module.eks_main.cluster_name}
-#       securityGroupSelectorTerms:
-#         - tags:
-#             karpenter.sh/discovery: ${module.eks_main.cluster_name}
-#       tags:
-#         karpenter.sh/discovery: ${module.eks_main.cluster_name}
-#   YAML
+resource "kubectl_manifest" "karpenter_node_class" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.k8s.aws/v1beta1
+    kind: EC2NodeClass
+    metadata:
+      name: autoscaling
+    spec:
+      amiFamily: Bottlerocket
+      role: ${module.karpenter.node_iam_role_name}
+      subnetSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${module.eks_main.cluster_name}
+      securityGroupSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${module.eks_main.cluster_name}
+      tags:
+        karpenter.sh/discovery: ${module.eks_main.cluster_name}
+  YAML
 
-#   depends_on = [
-#     helm_release.karpenter
-#   ]
-# }
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
 
-# resource "kubectl_manifest" "karpenter_node_pool" {
-#   yaml_body = <<-YAML
-#     apiVersion: karpenter.sh/v1beta1
-#     kind: NodePool
-#     metadata:
-#       name: default
-#     spec:
-#       template:
-#         spec:
-#           nodeClassRef:
-#             name: default
-#           requirements:
-#             - key: "karpenter.k8s.aws/instance-category"
-#               operator: In
-#               values: ["t","c", "m", "r"]
-#             - key: "karpenter.k8s.aws/instance-cpu"
-#               operator: In
-#               values: ["2", "4", "8", "16"]
-#             - key: "karpenter.k8s.aws/instance-hypervisor"
-#               operator: In
-#               values: ["nitro"]
-#             - key: "karpenter.k8s.aws/instance-generation"
-#               operator: Gt
-#               values: ["5"]
-#       limits:
-#         cpu: 1000
-#       disruption:
-#         consolidationPolicy: WhenEmpty
-#         consolidateAfter: 30s
-#   YAML
+resource "kubectl_manifest" "karpenter_node_pool" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.sh/v1beta1
+    kind: NodePool
+    metadata:
+      name: default
+    spec:
+      template:
+        spec:
+          nodeClassRef:
+            name: default
+          requirements:
+            - key: "karpenter.k8s.aws/instance-category"
+              operator: In
+              values: ["t","c", "m", "r"]
+            - key: "karpenter.k8s.aws/instance-cpu"
+              operator: In
+              values: ["2", "4", "8", "16"]
+            - key: "karpenter.k8s.aws/instance-hypervisor"
+              operator: In
+              values: ["nitro"]
+            - key: "karpenter.k8s.aws/instance-generation"
+              operator: Gt
+              values: ["5"]
+      limits:
+        cpu: 1000
+      disruption:
+        consolidationPolicy: WhenEmpty
+        consolidateAfter: 30s
+  YAML
 
-#   depends_on = [
-#     kubectl_manifest.karpenter_node_class
-#   ]
-# }
+  depends_on = [
+    kubectl_manifest.karpenter_node_class
+  ]
+}
 
-# # AWS required resources for Karpenter
-# module "eks_karpenter" {
-#   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
-#   version = "~> 20.31.0"
+# AWS required resources for Karpenter
+module "eks_karpenter" {
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "~> 20.31.0"
 
-#   cluster_name = module.eks_main.cluster_name
+  cluster_name = module.eks_main.cluster_name
 
-#   enable_v1_permissions = true
+  enable_v1_permissions = true
 
-#   enable_pod_identity             = true
-#   create_pod_identity_association = true
+  enable_pod_identity             = true
+  create_pod_identity_association = true
 
-#   # Used to attach additional IAM policies to the Karpenter node IAM role
-#   node_iam_role_additional_policies = {
-#     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-#   }
+  # Used to attach additional IAM policies to the Karpenter node IAM role
+  node_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
 module "cognito_pool" {
   source                       = "../../modules/cognito"
